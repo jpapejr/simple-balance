fe9306f71e975db5f6e6dbb1e4627a119d997195
John Pape <jpapejr@icloud.com>
2015-01-23 13:01:02 -0500

    Added a text case for BALANCE_METHOD_RDM

--- test-balance.js
+++ test-balance.js
@@ -5,14 +5,21 @@ if (sb){
 } else {
 	console.dir(sb)
 }
-var hosts = ["www.ibm.com:80", "blahZeeBlah:80",  "notawebsite:19923"]
+var hosts = [ "blahZeeBlah:80", "www.ibm.com:80", "notawebsite:19923"]
 
 var reqOpts = {
 	path: '/showmeanerror.html',
 	method: 'GET'
 }
 
-sb.sendBalancedRequest(hosts, sb.BALANCE_METHOD_RR, reqOpts, "sample payload", function(err, result){
+// sb.sendBalancedRequest(hosts, sb.BALANCE_METHOD_RR, reqOpts, "sample payload", function(err, result){
+// 	console.log("===> Errors:")
+// 	console.dir(err)
+// 	console.log("===> Result:")
+// 	console.dir(result)
+// })
+
+ sb.sendBalancedRequest(hosts, sb.BALANCE_METHOD_RDM, reqOpts, "sample payload", function(err, result){
 	console.log("===> Errors:")
 	console.dir(err)
 	console.log("===> Result:")
