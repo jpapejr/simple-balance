d2ff27b4bf82464d19dcdfc155bb34587ba692a9
John Pape <jpapejr@icloud.com>
2015-01-23 13:00:28 -0500

    Some docker housekeeping

--- docker-test.sh
+++ docker-test.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 clear
+#This line is simply to clean up any <none> docker images in your docker images list
+docker rmi `docker images -q -f dangling=true`
 docker build -t simple-balance-test .
 docker run --rm -it --name simple-balance-test simple-balance-test $1
